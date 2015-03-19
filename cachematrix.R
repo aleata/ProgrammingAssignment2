3b880670ef59eb00ff99f2d424ef1598305ab2de
aleata <aleata@gmail.com>
2015-03-18 23:06:31 -0700

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This script contains two functions that (a) store an object representing
+# a matrix and its mutator functions and (b) compute the inverse of a matrix 
+# or retrieve the matrix inverse from the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  # Creates an object that stores a matrix and its inverse
+  #
+  # Args:
+  #   x: A square matrix
+  #
+  # Returns:
+  #   A list of 4 functions that:
+  #   (1) set the value of a matrix, 
+  #   (2) get the value of the matrix,
+  #   (3) set the value of the matrix inverse,
+  #   (4) get the value of the matrix inverse
   m <- NULL
   set <- function(y) {
     x <<- y
@@ -18,9 +28,19 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+If the inverse has already been calculated (and the matrix has not changed), 
+then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+  # Compute or retrieve from the cache the inverse of the matrix returned by a makeCacheMatrix object
+  #
+  # Args:
+  #   x: A makeCacheMatrix object
+  #
+  # Returns:
+  #   The inverse of the matrix returned by x
+  #   If the inverse is retrieved from the cache, a message alerting the user is printed
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
